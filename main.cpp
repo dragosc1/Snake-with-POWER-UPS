ba0835af6136a7c1949ae1c6967d2c0a5f6cd239
dragosc1 <dragos.ciobanu232@gmail.com>
2023-03-04 11:44:52 +0200

    Layout for snake game

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Cell {
+private:
+    int x, y;
+public:
+    // getters si setters
+    int getX() {
+        return x;
+    }
+    int getY() {
+        return y;
+    }
+    void setX(int _x) {
+        x = _x;
+    }
+    void setY(int _y) {
+        y = _y;
+    }
+};
+
+class Window {
+
+};
+
+class Snake {
+    vector<Cell> body;
+
+};
+
+class World {
+
+};
 
 int main() {
-    std::cout << "Hello, world!\n";
+
     return 0;
 }
